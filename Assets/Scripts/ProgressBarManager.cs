659918089ccc1e19a2c54cb1d5fd7dafa9b18673
M-T-Asagi <t.asagi00+github@gmail.com>
2018-09-14 13:03:53 +0900

    プログレスバーが正常に動かない問題を修正

--- Assets/Scripts/ProgressBarManager.cs
+++ Assets/Scripts/ProgressBarManager.cs
@@ -13,24 +13,28 @@ public class ProgressBarManager : MonoBehaviour
 
     RectTransform rTransform;
     float max;
-    public float State { get; }
+
+    [SerializeField]
+    float state = 0;
+    public float State { get { return state; } }
 
     // Use this for initialization
     void Start()
     {
         rTransform = progressingBar.rectTransform;
-        max = baseBar.rectTransform.rect.width;
+        max = baseBar.rectTransform.sizeDelta.x;
     }
 
-    public void UpdateState(float state)
+    public void UpdateState(float _state)
     {
+        state = _state;
         if (state < 0)
             state = 0;
         else if (state > 1)
             state = 1;
 
-        Vector2 offsetMax = rTransform.offsetMax;
-        offsetMax.x = max * 0.5f - max * state;
-        rTransform.offsetMax = offsetMax;
+        Vector2 sizeDelta = rTransform.sizeDelta;
+        sizeDelta.x = max * state;
+        rTransform.sizeDelta = sizeDelta;
     }
 }
