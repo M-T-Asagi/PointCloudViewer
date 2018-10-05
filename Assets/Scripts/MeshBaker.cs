95fb99b61a8911792389b65027d42530e3ada8eb
M-T-Asagi <t.asagi00+github@gmail.com>
2018-10-05 20:53:10 +0900

    色々修正（みかん）

---
+++ Assets/Scripts/MeshBaker.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class MeshBaker : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefab;
+
+    [SerializeField]
+    PointCloudPTSViewer pTSViewer;
+
+    Vector3[] verticesBuff;
+    Color[] colorsBuff;
+    int[] indecesBuff;
+
+    bool process;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (process)
+        {
+            CreateChildObject();
+            pTSViewer.Restart();
+            process = false;
+        }
+    }
+
+    public void SetPoints(PointCloudPTSViewer.CloudPoint[] points)
+    {
+        CreateMesh(points);
+        process = true;
+    }
+
+    async void CreateMesh(PointCloudPTSViewer.CloudPoint[] points)
+    {
+        Debug.Log("creating meshes start!");
+        await Task.Run(() =>
+        {
+            int _pointCount = points.Length;
+
+            verticesBuff = new Vector3[_pointCount];
+            colorsBuff = new Color[_pointCount];
+            indecesBuff = new int[_pointCount];
+
+            for (int i = 0; i < _pointCount; i++)
+            {
+                verticesBuff[i] = points[i].point;
+                colorsBuff[i] = points[i].color;
+                indecesBuff[i] = i;
+            }
+        });
+    }
+
+    void CreateChildObject()
+    {
+        Debug.Log("creating child objects start!");
+        Mesh mesh = new Mesh();
+        mesh.vertices = verticesBuff;
+        mesh.colors = colorsBuff;
+        mesh.SetIndices(indecesBuff, MeshTopology.Points, 0);
+
+        GameObject child = Instantiate(prefab, transform);
+        child.GetComponent<MeshFilter>().sharedMesh = mesh;
+
+        Cleanup();
+    }
+
+    private void OnDestroy()
+    {
+        Cleanup();
+    }
+
+    void Cleanup()
+    {
+        verticesBuff = null;
+        colorsBuff = null;
+        indecesBuff = null;
+    }
+}
--- Assets/Scripts/PointCloudPTSViewer.cs
+++ Assets/Scripts/PointCloudPTSViewer.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 public class PointCloudPTSViewer : MonoBehaviour
 {
-    const int MAX_LIST_LENGTH = 500000;
+    const int MAX_LIST_LENGTH = 300000;
 
-    struct CloudPoint
+    public struct CloudPoint
     {
         public Vector3 point;
         public int intensity;
@@ -32,28 +33,12 @@ public class PointCloudPTSViewer : MonoBehaviour
         }
     }
 
-    enum State
-    {
-        Setup = 0,
-        WaitStartScan,
-        Scanning,
-        WaitStartCreateMeshes,
-        CreatingMeshes,
-        WaitStartBaking,
-        Baking,
-        FinishedBaking,
-        FinishedAllProcess,
-        Destroyed,
-
-        ItemNum
-    }
-
     [SerializeField]
     float sizeScale = 0.0001f;
     [SerializeField]
     string filePath;
     [SerializeField]
-    GameObject prefab;
+    MeshBaker meshBaker;
     [SerializeField]
     Text textArea;
     [SerializeField]
@@ -61,24 +46,23 @@ public class PointCloudPTSViewer : MonoBehaviour
     [SerializeField]
     GameObject canvas;
 
-    CloudPoint[] points;
-
     int pointNum = -1;
     int processedPointNum = 0;
+    int processedCount = 0;
     StreamReader reader = null;
+    bool continuos = true;
+    bool destroy = false;
 
-    State state = 0;
-    bool continuos = false;
-
-    Vector3[] verticesBuff;
-    Color[] colorsBuff;
-    int[] indecesBuff;
+    ParallelOptions options;
 
     // Use this for initialization
     void Start()
     {
         if (filePath == null || filePath == "")
             return;
+
+        options = new ParallelOptions();
+        options.MaxDegreeOfParallelism = 4;
 
         SetupPointScaning(filePath);
     }
@@ -88,7 +72,7 @@ public class PointCloudPTSViewer : MonoBehaviour
         reader = new StreamReader(path);
         string fl = reader.ReadLine();
         pointNum = Int32.Parse(fl);
-        state++;
+        Restart();
     }
 
     // Update is called once per frame
@@ -99,142 +83,110 @@ public class PointCloudPTSViewer : MonoBehaviour
             pbManager.UpdateState((float)processedPointNum / (float)pointNum);
             textArea.text = processedPointNum + " /\n" + pointNum;
         }
+    }
 
-        switch (state)
-        {
-            case State.WaitStartScan:
-                Task.Run(() => SetPointsAsync()).ContinueWith((Task t) =>
-                {
-                    Debug.Log("Setting points process is finished!");
-                    state++;
-                });
-                state++;
-                break;
-            case State.Scanning:
-                break;
-            case State.WaitStartCreateMeshes:
-                CreateMesh();
-                state++;
-                break;
-            case State.CreatingMeshes:
-                break;
-            case State.WaitStartBaking:
-                CreateChildObject();
-                break;
-            case State.Baking:
-                state++;
-                break;
-            case State.FinishedBaking:
-                if (continuos)
-                {
-                    state = State.WaitStartScan;
-                }
-                else
-                {
-                    state = State.FinishedAllProcess;
-                }
-                break;
-            case State.FinishedAllProcess:
-                Cleanup();
-                canvas.SetActive(false);
-                break;
-        }
+    async void CallSetPoint()
+    {
+        await Task.Run(() => SetPointsAsync());
+        processedCount++;
+
+        Debug.Log("Setting points process is finished!");
+        Debug.Log("Processed count : " + processedCount);
     }
 
     void SetPointsAsync()
     {
         Debug.Log("Scan start!");
 
-        int newPointsArrayCount = Mathf.Min(MAX_LIST_LENGTH, pointNum - processedPointNum);
+        int newPointsArrayCount = Mathf.Min(MAX_LIST_LENGTH, pointNum - MAX_LIST_LENGTH * (processedCount + 1));
+        Debug.Log("new points array count : " + newPointsArrayCount);
+
         continuos = (newPointsArrayCount >= MAX_LIST_LENGTH);
 
-        points = new CloudPoint[newPointsArrayCount];
-        ParallelOptions options = new ParallelOptions();
-        options.MaxDegreeOfParallelism = 4;
+        CloudPoint[] points = new CloudPoint[newPointsArrayCount];
 
+        int currentCount = 0;
         Parallel.For(0, newPointsArrayCount, options, async (i, loopState) =>
         {
-            string _read = await reader.ReadLineAsync();
-            string[] data = _read.Split(' ');
-            points[i] = new CloudPoint(
-                new Vector3(
-                    float.Parse(data[0]) * sizeScale,
-                    float.Parse(data[1]) * sizeScale,
-                    float.Parse(data[2]) * sizeScale
-                ),
-                Int32.Parse(data[3]),
-                new Color(
-                    float.Parse(data[4]) / 255f,
-                    float.Parse(data[5]) / 255f,
-                    float.Parse(data[6]) / 255f
-                ));
-            processedPointNum++;
+            try
+            {
+                string _read = await reader.ReadLineAsync();
+                if (_read == null || _read == "")
+                {
+                    Debug.LogError("reading failed!");
+                }
+                else
+                {
+                    string[] data = _read.Split(' ');
+                    points[i] = new CloudPoint(
+                        new Vector3(
+                            float.Parse(data[0]) * sizeScale,
+                            float.Parse(data[1]) * sizeScale,
+                            float.Parse(data[2]) * sizeScale
+                        ),
+                        Int32.Parse(data[3]),
+                        new Color(
+                            float.Parse(data[4]) / 255f,
+                            float.Parse(data[5]) / 255f,
+                            float.Parse(data[6]) / 255f
+                        ));
 
-            if (state == State.Destroyed)
+                    if (i == 0)
+                    {
+                        Debug.Log("akakakakakakakaka");
+                        Debug.Log(points[i]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                Debug.LogException(e);
+                Debug.LogError("Dead!!!!!!!!!!!!!");
+            }
+
+
+            if (destroy)
             {
                 loopState.Stop();
-                Cleanup();
                 return;
             }
-        });
-    }
 
-
-    async void CreateMesh()
-    {
-        Debug.Log("creating meshes start!");
-        await Task.Run(() =>
-        {
-            int _pointCount = points.Length;
-
-            verticesBuff = new Vector3[_pointCount];
-            colorsBuff = new Color[_pointCount];
-            indecesBuff = new int[_pointCount];
-
-            for (int i = 0; i < _pointCount; i++)
+            lock (Thread.CurrentContext)
             {
-                verticesBuff[i] = points[i].point;
-                colorsBuff[i] = points[i].color;
-                indecesBuff[i] = i;
+                processedPointNum++;
             }
-            state++;
+            currentCount++;
         });
-    }
 
-    void CreateChildObject()
-    {
-        Debug.Log("creating child objects start!");
-        Mesh mesh = new Mesh();
-        mesh.vertices = verticesBuff;
-        mesh.colors = colorsBuff;
-        mesh.SetIndices(indecesBuff, MeshTopology.Points, 0);
-
-        GameObject child = Instantiate(prefab, transform);
-        child.GetComponent<MeshFilter>().sharedMesh = mesh;
-        state++;
+        Debug.Log("processed! : " + currentCount + "/" + points.Length);
+        meshBaker.SetPoints(points);
     }
 
     private void OnDestroy()
     {
-        state = State.Destroyed;
+        destroy = true;
+        Cleanup();
+    }
+
+    public void Restart()
+    {
+        if (continuos)
+        {
+            CallSetPoint();
+        }
+        else
+        {
+            Cleanup();
+            canvas.SetActive(false);
+        }
     }
 
     void Cleanup()
     {
         Debug.Log("cleaning up!");
-        verticesBuff = null;
-        colorsBuff = null;
-        indecesBuff = null;
+
         reader.Close();
-    }
-
-    public void Build(string filepath)
-    {
-        if (state == State.Setup || state == State.FinishedAllProcess)
-        {
-            state = State.Setup;
-            SetupPointScaning(filepath);
-        }
-
+        Destroy(this);
     }
 }
