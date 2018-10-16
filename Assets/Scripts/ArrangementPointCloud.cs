ad567bef4ae631ea1769f7de4d1d3bf9ee7d3c69
M-T-Asagi <t.asagi00+github@gmail.com>
2018-10-16 14:19:44 +0900

    IndexedVector3ヲ別ファイル化

--- Assets/Scripts/ArrangementPointCloud.cs
+++ Assets/Scripts/ArrangementPointCloud.cs
@@ -7,35 +7,6 @@ using UnityEngine;
 
 public class ArrangementPointCloud : MonoBehaviour
 {
-    class IndexedVector3 : IEquatable<IndexedVector3>
-    {
-        public int x;
-        public int y;
-        public int z;
-
-        public IndexedVector3(int _x, int _y, int _z)
-        {
-            x = _x;
-            y = _y;
-            z = _z;
-        }
-
-        public override int GetHashCode()
-        {
-            return x.GetHashCode() ^ y.GetHashCode() ^ z.GetHashCode();
-        }
-
-        bool IEquatable<IndexedVector3>.Equals(IndexedVector3 other)
-        {
-            if (other == null || x != other.x || y != other.y || z != other.z)
-            {
-                return false;
-            }
-
-            return true;
-        }
-    }
-
     [SerializeField]
     List<Mesh> meshes;
 
---
+++ Assets/Scripts/IndexedVector3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class IndexedVector3 : IEquatable<IndexedVector3>
+{
+    public int x;
+    public int y;
+    public int z;
+
+    public IndexedVector3(int _x, int _y, int _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+
+    public override int GetHashCode()
+    {
+        return x.GetHashCode() ^ y.GetHashCode() ^ z.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return "x: " + x + "\ny: " + y + "\nz: " + z;
+    }
+
+    bool IEquatable<IndexedVector3>.Equals(IndexedVector3 other)
+    {
+        if (other == null || x != other.x || y != other.y || z != other.z)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Vector3 ToVector3()
+    {
+        return new Vector3(x, y, z);
+    }
+}
