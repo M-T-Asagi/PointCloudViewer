93cb80e8639574c0bbb87e4447d8ac6c0768ae2d
M-T-Asagi <t.asagi00+github@gmail.com>
2018-10-19 19:03:44 +0900

    Create collectingPoints script.(TODO)

---
+++ Assets/Scripts/CollectingPoints.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CollectingPoints : MonoBehaviour
+{
+    [SerializeField]
+    float cubeSize = 0.1f;
+    [SerializeField]
+    string filePath;
+    [SerializeField]
+    PtsToCloudPointConverter converter;
+    [SerializeField]
+    MeshBaker baker;
+    [SerializeField]
+    MeshSaver saver;
+    [SerializeField]
+    ProgressBarManager pbManager;
+
+    GameObject meshesRoot;
+    Dictionary<IndexedVector3, Color> collectedPoints;
+    ParallelOptions options;
+
+    bool allProcessIsUp = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        collectedPoints = new Dictionary<IndexedVector3, Color>();
+
+        options = new ParallelOptions();
+        options.MaxDegreeOfParallelism = 4;
+
+        converter.SetupPointScaning(filePath);
+
+        meshesRoot = new GameObject();
+        meshesRoot.transform.parent = transform;
+        baker.SetUp(meshesRoot.transform);
+    }
+
+
+    void CallConverterProcess()
+    {
+        converter.Process();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!allProcessIsUp && converter.TotalPointCount >= 0)
+        {
+            pbManager.UpdateState((float)converter.ProcessedPointCount / (float)converter.TotalPointCount);
+            pbManager.UpdateStateText(converter.ProcessedPointCount + " /\n" + converter.TotalPointCount);
+        }
+    }
+
+    void ProcessUp(object sender, PtsToCloudPointConverter.ProcessUpArgs args)
+    {
+        CallCollecting(args.cloudPoints);
+    }
+
+    async void CallCollecting(CloudPoint[] _points)
+    {
+        CloudPoint[] points = new CloudPoint[_points.Length];
+        Array.Copy(_points, points, _points.Length);
+        await Task.Run(() => Collecting(points));
+        CallConverterProcess();
+    }
+
+    void Collecting(CloudPoint[] points)
+    {
+        Parallel.For(0, points.Length, options, (i, loopState) =>
+        {
+            IndexedVector3 newIndex = new IndexedVector3(
+                Mathf.RoundToInt(points[i].point.x / cubeSize),
+                Mathf.RoundToInt(points[i].point.y / cubeSize),
+                Mathf.RoundToInt(points[i].point.z / cubeSize)
+                );
+
+            if (!collectedPoints.ContainsKey(newIndex))
+            {
+                lock (Thread.CurrentContext)
+                    collectedPoints.Add(newIndex, points[i].color);
+            }
+        });
+    }
+
+    void AllProcessUp()
+    {
+
+    }
+}
