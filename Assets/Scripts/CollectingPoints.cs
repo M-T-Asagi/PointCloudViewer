ac9e6119512cc72ebbe51125ae49f476ef494a6e
M-T-Asagi <t.asagi00+github@gmail.com>
2018-10-22 16:00:32 +0900

    色々使えるよう修正

--- Assets/Scripts/CollectingPoints.cs
+++ Assets/Scripts/CollectingPoints.cs
@@ -14,6 +14,8 @@ public class CollectingPoints : MonoBehaviour
     [SerializeField]
     PtsToCloudPointConverter converter;
     [SerializeField]
+    PointsArranger arranger;
+    [SerializeField]
     MeshBaker baker;
     [SerializeField]
     MeshSaver saver;
@@ -25,6 +27,7 @@ public class CollectingPoints : MonoBehaviour
     ParallelOptions options;
 
     bool allProcessIsUp = false;
+    bool destroyed = false;
 
     // Use this for initialization
     void Start()
@@ -39,22 +42,19 @@ public class CollectingPoints : MonoBehaviour
         meshesRoot = new GameObject();
         meshesRoot.transform.parent = transform;
         baker.SetUp(meshesRoot.transform);
-    }
 
+        converter.processUp += ProcessUp;
+        converter.allProcessUp += AllProcessUp;
+        arranger.finishProcess += ArrangingProcessUp;
+        baker.finishGenerate += MeshesGenerated;
+        baker.finishBaking += MeshesBaked;
+
+        CallConverterProcess();
+    }
 
     void CallConverterProcess()
     {
         converter.Process();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!allProcessIsUp && converter.TotalPointCount >= 0)
-        {
-            pbManager.UpdateState((float)converter.ProcessedPointCount / (float)converter.TotalPointCount);
-            pbManager.UpdateStateText(converter.ProcessedPointCount + " /\n" + converter.TotalPointCount);
-        }
     }
 
     void ProcessUp(object sender, PtsToCloudPointConverter.ProcessUpArgs args)
@@ -85,11 +85,75 @@ public class CollectingPoints : MonoBehaviour
                 lock (Thread.CurrentContext)
                     collectedPoints.Add(newIndex, points[i].color);
             }
+
+            if (destroyed)
+            {
+                loopState.Stop();
+                return;
+            }
         });
     }
 
-    void AllProcessUp()
+    void AllProcessUp(object sender, PtsToCloudPointConverter.AllProcessUpArgs args)
     {
+        CallArranging();
+    }
 
+    void CallArranging()
+    {
+        CloudPoint[] points = new CloudPoint[collectedPoints.Count];
+        int count = 0;
+
+        foreach (KeyValuePair<IndexedVector3, Color> point in collectedPoints)
+        {
+            points[count] = new CloudPoint(point.Key.ToVector3(), 1, point.Value);
+            count++;
+        }
+
+        arranger.Process(points);
+    }
+
+    void ArrangingProcessUp(object sender, PointsArranger.FinishProcessArgs args)
+    {
+        List<CloudPoint[]> points = new List<CloudPoint[]>();
+        List<Vector3> centers = new List<Vector3>();
+        foreach (KeyValuePair<IndexedVector3, List<CloudPoint>> val in args.chunkedPositions)
+        {
+            CloudPoint[] _points = val.Value.ToArray();
+            centers.Add(val.Key.ToVector3() * arranger.ChunkSize);
+            points.Add(_points);
+        }
+
+        baker.SetPoints(points, centers);
+    }
+
+    void MeshesGenerated(object sender, MeshBaker.FinishGenerateArgs args)
+    {
+        baker.SetMeshToBake(args.centers, args.meshes);
+    }
+
+    void MeshesBaked(object sender, MeshBaker.FinishBakingArgs args)
+    {
+        saver.Process(meshesRoot);
+    }
+
+    void MeshesSaved(object sender, EventArgs args)
+    {
+        pbManager.Finish();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!allProcessIsUp && converter.TotalPointCount >= 0 && pbManager != null)
+        {
+            pbManager.UpdateState((float)converter.ProcessedPointCount / (float)converter.TotalPointCount);
+            pbManager.UpdateStateText(converter.ProcessedPointCount + " /\n" + converter.TotalPointCount);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        destroyed = true;
     }
 }
