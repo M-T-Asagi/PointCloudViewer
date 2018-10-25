41a517c2292a7083cb5b8bc1a5f1075a5429fd72
M-T-Asagi <t.asagi00+github@gmail.com>
2018-10-25 20:42:04 +0900

    Cubing処理を追加

---
+++ Assets/Scripts/PointsToCube.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class PointsToCube : MonoBehaviour
+{
+    public class FinishGeneratingEventArgs : EventArgs
+    {
+        public Mesh[] generatedMeshes;
+
+        public FinishGeneratingEventArgs(Mesh[] _generatedMshes)
+        {
+            generatedMeshes = (Mesh[])_generatedMshes.Clone();
+        }
+    }
+
+    [SerializeField]
+    Mesh mesh;
+
+    public EventHandler<FinishGeneratingEventArgs> finish;
+
+    Vector3[] prefabPoints;
+    int[] prefabTriangles;
+
+    bool generating = false;
+
+    List<CloudPoint[]> points;
+
+    // Use this for initialization
+    void Start()
+    {
+        GeneratePrefavPoints();
+    }
+
+    void GeneratePrefavPoints()
+    {
+        prefabPoints = (Vector3[])mesh.vertices.Clone();
+        prefabTriangles = (int[])mesh.triangles.Clone();
+        Vector3 center = Vector3.zero;
+        float max = 0;
+
+        for (int i = 0; i < prefabPoints.Length; i++)
+        {
+            if (i == 0)
+                center = prefabPoints[i];
+            else
+                center = (center + prefabPoints[i]) / 2f;
+        }
+
+        for (int i = 0; i < prefabPoints.Length; i++)
+        {
+            prefabPoints[i] -= center;
+        }
+
+        for (int i = 0; i < prefabPoints.Length; i++)
+        {
+            if (prefabPoints[i].x > max) max = Mathf.Abs(prefabPoints[i].x);
+            if (prefabPoints[i].y > max) max = Mathf.Abs(prefabPoints[i].y);
+            if (prefabPoints[i].z > max) max = Mathf.Abs(prefabPoints[i].z);
+        }
+
+        for (int i = 0; i < prefabPoints.Length; i++)
+        {
+            prefabPoints[i] /= max;
+        }
+    }
+
+    public void Process(CloudPoint[] _points)
+    {
+        points = new List<CloudPoint[]>() { (CloudPoint[])_points.Clone() };
+        generating = true;
+    }
+
+    public void Process(List<CloudPoint[]> _points)
+    {
+        Debug.Log("Cubing received!");
+        points = new List<CloudPoint[]>(_points);
+        generating = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (generating)
+        {
+            GenerateMeshes();
+            generating = false;
+        }
+    }
+
+    void GenerateMeshes()
+    {
+        Debug.Log("Start generate meshes! / " + points.Count);
+        Mesh[] meshes = new Mesh[points.Count];
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            meshes[i] = _GenerateMeshes(points[i]);
+        }
+        Debug.Log("Finished generate meshes!");
+
+        finish?.Invoke(this, new FinishGeneratingEventArgs(meshes));
+    }
+
+    Mesh _GenerateMeshes(CloudPoint[] _points)
+    {
+        Mesh mesh = new Mesh();
+        Vector3[] vertices = new Vector3[prefabPoints.Length * _points.Length];
+        int[] triangles = new int[prefabTriangles.Length * _points.Length];
+        Color[] colors = new Color[prefabPoints.Length * _points.Length];
+
+        Debug.Log("Process " + _points.Length + " points!");
+
+        for (int i = 0; i < _points.Length; i++)
+        {
+            Array.Copy(GeneratePointedVertex(_points[i].point), 0, vertices, i * prefabPoints.Length, prefabPoints.Length);
+            Array.Copy(prefabTriangles, 0, triangles, i * prefabTriangles.Length, prefabTriangles.Length);
+            Array.Copy(GenerateColor(_points[i].color), 0, colors, i * prefabPoints.Length, prefabPoints.Length);
+        }
+
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.colors = colors;
+
+        return mesh;
+    }
+
+    Vector3[] GeneratePointedVertex(Vector3 point)
+    {
+        Vector3[] pointsBuff = (Vector3[])prefabPoints.Clone();
+        for (int i = 0; i < pointsBuff.Length; i++)
+        {
+            pointsBuff[i] += point;
+        }
+        return pointsBuff;
+    }
+
+    Color[] GenerateColor(Color color)
+    {
+        Color[] colors = new Color[prefabPoints.Length];
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i] = color;
+        }
+        return colors;
+    }
+}
---
+++ Assets/Scripts/PtsToCubingManager.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PtsToCubingManager : MonoBehaviour
+{
+
+    public enum State
+    {
+        Settings = 0,
+        Converting,
+        Collecting,
+        Bundling,
+        Arranging,
+        Cubing,
+        Generating,
+        Baking,
+        Saving,
+
+        ItemNum
+    }
+
+    [SerializeField]
+    float cubeSize = 0.1f;
+    [SerializeField]
+    string filePath;
+    [SerializeField]
+    PtsToCloudPointConverter converter;
+    [SerializeField]
+    PointsArranger arranger;
+    [SerializeField]
+    MeshBaker baker;
+    [SerializeField]
+    MeshSaver saver;
+    [SerializeField]
+    PointsToCube cuber;
+    [SerializeField]
+    ProgressBarManager pbManager;
+    [SerializeField]
+    ObjectActiveManager pbManagerActiveManager;
+    [SerializeField]
+    ProgressBarManager subpbManager;
+    [SerializeField]
+    ObjectActiveManager subPBManagerActiveManager;
+    [SerializeField]
+    Text stateText;
+
+
+    GameObject meshesRoot;
+    Dictionary<IndexedVector3, Color> collectedPoints;
+    ParallelOptions options;
+
+    Vector3[] centers;
+
+    State stateNow = 0;
+
+    bool allProcessIsUp = false;
+    bool destroyed = false;
+
+    int subCount = 0;
+    int subAll = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        collectedPoints = new Dictionary<IndexedVector3, Color>();
+
+        options = new ParallelOptions();
+        options.MaxDegreeOfParallelism = 4;
+
+        converter.SetupPointScaning(filePath);
+
+        meshesRoot = new GameObject();
+        meshesRoot.transform.parent = transform;
+        baker.SetUp(meshesRoot.transform);
+
+        converter.processUp += ProcessUp;
+        converter.allProcessUp += AllProcessUp;
+        arranger.finishProcess += ArrangingProcessUp;
+        cuber.finish += CubingProcessUp;
+        baker.finishBaking += MeshesBaked;
+
+        CallConverterProcess();
+    }
+
+    void CallConverterProcess()
+    {
+        converter.Process();
+        stateNow = State.Converting;
+    }
+
+    void ProcessUp(object sender, PtsToCloudPointConverter.ProcessUpArgs args)
+    {
+        CallCollecting(args.cloudPoints);
+    }
+
+    async void CallCollecting(CloudPoint[] _points)
+    {
+        CloudPoint[] points = (CloudPoint[])_points.Clone();
+
+        subCount = 0;
+        subAll = points.Length;
+
+        stateNow = State.Collecting;
+
+        await Task.Run(() => Collecting(points));
+        CallConverterProcess();
+    }
+
+    void Collecting(CloudPoint[] points)
+    {
+        Parallel.For(0, points.Length, options, (i, loopState) =>
+        {
+            try
+            {
+                IndexedVector3 newIndex = new IndexedVector3(
+                    Mathf.RoundToInt(points[i].point.x / cubeSize),
+                    Mathf.RoundToInt(points[i].point.y / cubeSize),
+                    Mathf.RoundToInt(points[i].point.z / cubeSize)
+                    );
+
+                if (!collectedPoints.ContainsKey(newIndex))
+                {
+                    lock (Thread.CurrentContext)
+                        collectedPoints.Add(newIndex, points[i].color);
+                }
+
+                lock (Thread.CurrentContext)
+                    subCount++;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                Debug.LogException(e);
+                Debug.LogError("Collecting process is Dead!!!!!!!!!!!!!");
+            }
+
+            if (destroyed)
+            {
+                loopState.Stop();
+                return;
+            }
+        });
+    }
+
+    void AllProcessUp(object sender, PtsToCloudPointConverter.AllProcessUpArgs args)
+    {
+        CallBundlingPoints();
+    }
+
+    async void CallBundlingPoints()
+    {
+        stateNow = State.Bundling;
+
+        List<CloudPoint> t = await Task.Run(() => BundlingPoints());
+        CallArrange(t.ToArray());
+    }
+
+    List<CloudPoint> BundlingPoints()
+    {
+        List<CloudPoint> points = new List<CloudPoint>();
+        subCount = 0;
+        subAll = collectedPoints.Count;
+        Parallel.ForEach(collectedPoints, options, (point, loopState) =>
+        {
+            try
+            {
+                lock (Thread.CurrentContext)
+                    points.Add(new CloudPoint(point.Key.ToVector3(), 1, point.Value));
+                lock (Thread.CurrentContext)
+                    subCount++;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                Debug.LogException(e);
+                Debug.LogError("Bundling points process is Dead!!!!!!!!!!!!!");
+            }
+
+            if (destroyed)
+            {
+                loopState.Stop();
+                return;
+            }
+        });
+        return points;
+    }
+
+    void CallArrange(CloudPoint[] _points)
+    {
+        stateNow = State.Arranging;
+        CloudPoint[] points = (CloudPoint[])_points.Clone();
+        arranger.Process(points);
+    }
+
+    void ArrangingProcessUp(object sender, PointsArranger.FinishProcessArgs args)
+    {
+        List<CloudPoint[]> points = new List<CloudPoint[]>();
+        List<Vector3> _centers = new List<Vector3>();
+        foreach (KeyValuePair<IndexedVector3, List<CloudPoint>> val in args.chunkedPositions)
+        {
+            CloudPoint[] _points = val.Value.ToArray();
+            _centers.Add(val.Key.ToVector3() * arranger.ChunkSize);
+            points.Add(_points);
+        }
+        CallPointsToCube(points, _centers.ToArray());
+    }
+
+    void CallPointsToCube(List<CloudPoint[]> _points, Vector3[] _centers)
+    {
+        stateNow = State.Cubing;
+        centers = (Vector3[])_centers.Clone();
+        Debug.Log("Centers num is " + centers.Length);
+        cuber.Process(_points);
+    }
+
+    void CubingProcessUp(object sender, PointsToCube.FinishGeneratingEventArgs args)
+    {
+        baker.SetMeshToBake(centers, args.generatedMeshes);
+    }
+
+    void MeshesBaked(object sender, MeshBaker.FinishBakingArgs args)
+    {
+        stateNow = State.Saving;
+        saver.Process(meshesRoot);
+    }
+
+    void MeshesSaved(object sender, EventArgs args)
+    {
+        pbManager.Finish();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateText.text = "State now:\n    " + stateNow.ToString();
+        UpdateMainProgressBar();
+        UpdateSubProgressbar();
+    }
+
+    void UpdateMainProgressBar()
+    {
+        pbManagerActiveManager.Active = true;
+        switch (stateNow)
+        {
+            case State.Converting:
+                pbManager.UpdateState((float)converter.ProcessedPointCount / (float)converter.TotalPointCount);
+                pbManager.UpdateStateText(converter.ProcessedPointCount + " /\n" + converter.TotalPointCount);
+                break;
+            case State.Collecting:
+                break;
+            case State.Bundling:
+                pbManager.UpdateState((float)subCount / (float)subAll);
+                pbManager.UpdateStateText(subCount + " /\n" + subAll);
+                break;
+            case State.Arranging:
+                pbManager.UpdateState((float)arranger.ProcessedPointCount / (float)arranger.AllPointCount);
+                pbManager.UpdateStateText(arranger.ProcessedPointCount + " /\n" + arranger.AllPointCount);
+                break;
+            default:
+                pbManagerActiveManager.Active = false;
+                break;
+        }
+    }
+
+    void UpdateSubProgressbar()
+    {
+        subPBManagerActiveManager.Active = true;
+        switch (stateNow)
+        {
+            case State.Collecting:
+                subpbManager.UpdateState((float)subCount / (float)subAll);
+                subpbManager.UpdateStateText(subCount + "/\n" + subAll);
+                break;
+            default:
+                subPBManagerActiveManager.Active = false;
+                break;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        destroyed = true;
+    }
+}
