8720fa3ea9d25df8b24e3abecb89f0c7effc5098
M-T-Asagi <t.asagi00+github@gmail.com>
2018-11-07 03:20:19 +0900

    add Serialize scripts

---
+++ Assets/Scripts/Serialize.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/** Summary
+ * 
+ * Copied by https://qiita.com/k_yanase/items/fb64ccfe1c14567a907d from @k_yanase
+ * 
+ **/
+
+namespace Serialize
+{
+    /// <summary>
+    /// テーブルの管理クラス
+    /// </summary>
+    [System.Serializable]
+    public class TableBase<TKey, TValue, Type> where Type : KeyAndValue<TKey, TValue>
+    {
+        [SerializeField]
+        private List<Type> list;
+        private Dictionary<TKey, TValue> table;
+
+        public Dictionary<TKey, TValue> GetTable()
+        {
+            if (table == null)
+            {
+                table = ConvertListToDictionary(list);
+            }
+            return table;
+        }
+
+        /// <summary>
+        /// Editor Only
+        /// </summary>
+        public List<Type> GetList()
+        {
+            return list;
+        }
+
+        static Dictionary<TKey, TValue> ConvertListToDictionary(List<Type> list)
+        {
+            Dictionary<TKey, TValue> dic = new Dictionary<TKey, TValue>();
+            foreach (KeyAndValue<TKey, TValue> pair in list)
+            {
+                dic.Add(pair.Key, pair.Value);
+            }
+            return dic;
+        }
+    }
+
+    /// <summary>
+    /// シリアル化できる、KeyValuePair
+    /// </summary>
+    [System.Serializable]
+    public class KeyAndValue<TKey, TValue>
+    {
+        public TKey Key;
+        public TValue Value;
+
+        public KeyAndValue(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+        }
+        public KeyAndValue(KeyValuePair<TKey, TValue> pair)
+        {
+            Key = pair.Key;
+            Value = pair.Value;
+        }
+
+
+    }
+}
